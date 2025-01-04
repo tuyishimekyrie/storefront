3380904159c4e47ad0f3b718cd1eb3e56009a9a9
tuyishimekyrie <tuyishimehope01@gmail.com>
2025-01-04 20:38:30 +0200

    Add models in the apps

--- likes/models.py
+++ likes/models.py
@@ -1,12 +1,10 @@
 from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.auth.models import User
 # Create your models here.
-class Tag(models.Model):
-    title = models.CharField(max_length=255)
-    
-class TagItem(models.Model):
-    tag = models.ForeignKey(Tag,on_delete=models.CASCADE)
+class LikedItem(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
     content_type = models.ForeignKey(ContentType,on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey()
--- store/models.py
+++ store/models.py
@@ -1,18 +1,71 @@
 from django.db import models
 
 # Create your models here.
+class Promotion(models.Model):
+    description = models.CharField(max_length = 255)
+    discount = models.FloatField()
+    
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
+    featured_product = models.ForeignKey('Product',on_delete=models.SET_NULL,null=True,related_name='+')
+
 class Product(models.Model):
-    title = models.charField(max_length=255)
+    title = models.CharField(max_length=255)
+    slug = models.SlugField()
     description = models.TextField()
-    price = models.DecimalField(max_digits=6,decimal_places=2)
+    unit_price = models.DecimalField(max_digits=6,decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
+    collection = models.ForeignKey(Collection,on_delete=models.PROTECT)
+    promotions = models.ManyToManyField(Promotion)
     
 class Customer(models.Model):
+    MEMBERSHIP_BRONZE = 'B'
+    MEMBERSHIP_SILVER = 'S'
+    MEMBERSHIP_GOLD = 'G'
+    
+    Membership_choices = [
+        (MEMBERSHIP_BRONZE,'Bronze'),
+        (MEMBERSHIP_SILVER,'Silver'),
+        (MEMBERSHIP_GOLD,'Gold'),
+    ]
     first_name = models.CharField(max_length=150)
     last_name = models.CharField(max_length=150)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=15)
     birth_date = models.DateField(null=True)
+    membership = models.CharField(max_length = 100,choices=Membership_choices,default=MEMBERSHIP_BRONZE)
     
     
+class Order(models.Model):
+    PAYMENT_PENDING = 'P'
+    PAYMENT_COMPLETED = 'C'
+    PAYMENT_FAILED = 'F'
+    payment_status_choices = [
+        (PAYMENT_PENDING,'Pending'),
+        (PAYMENT_COMPLETED,'Complete'),
+        (PAYMENT_FAILED,'Failed'),
+    ]
+    placed_at = models.DateTimeField(auto_now_add = True)
+    payment_status = models.CharField(max_length=100,choices=payment_status_choices,default=PAYMENT_PENDING)
+    customer = models.ForeignKey(Customer,on_delete=models.PROTECT)
+    
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order,on_delete=models.PROTECT)
+    product = models.ForeignKey(Product,on_delete=models.PROTECT)
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6,decimal_places=2)
+
+class Address(models.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    # customer = models.OneToOneField(Customer,on_delete=models.CASCADE,primary_key=True)
+    customer = models.ForeignKey(Customer,on_delete=models.CASCADE)
+
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class CartItem(models.Model):
+    cart = models.ForeignKey('Cart',on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
