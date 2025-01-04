b0d087327b9809512ac547b1b3e3654fad3fce89
tuyishimekyrie <tuyishimehope01@gmail.com>
2025-01-04 14:37:33 +0200

    Add store and tags apps

---
+++ store/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+    title = models.charField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6,decimal_places=2)
+    inventory = models.IntegerField()
+    last_update = models.DateTimeField(auto_now=True)
+    
+class Customer(models.Model):
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15)
+    birth_date = models.DateField(null=True)
+    
+    
