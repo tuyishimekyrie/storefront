bd3ffd44631463d330d8a51a58fc092915e4ca03
tuyishimekyrie <tuyishimehope01@gmail.com>
2025-01-04 12:25:30 +0200

    Add templates directory

--- playground/views.py
+++ playground/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def hello_world(request):
-    return HttpResponse("Hello World!")
+    return render(request, "hello.html")
 
 # Create your views here.
