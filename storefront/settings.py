f094534ef56fec61948f672e04b05a0a63f5f674
tuyishimekyrie <tuyishimehope01@gmail.com>
2025-01-05 16:00:59 +0200

    Add migrations directories in apps

--- storefront/settings.py
+++ storefront/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'storefront',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
